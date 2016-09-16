c24a84014a300ac504092cd39d3e25a8b1c55f62
Adamowski, Billy <Adamowski.Billy@principal.com>
2016-09-16 11:50:06 -0500

    Trying ads again

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('trackamon', ['ngRoute']);
+var app = angular.module('trackamon', ['ngRoute', 'angular-google-adsense']);
 
 app.controller('ParentController', function($scope) {
 
